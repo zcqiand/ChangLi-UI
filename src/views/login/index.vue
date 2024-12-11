aba4d6f2edbfeefa9efeab01d73bb0684db025bc
zcqiand <zcqiand@163.com>
2024-12-11 20:24:11 +0800

    重构

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,26 +1,48 @@
 <template>
-	<div class="login-container flx-center">
-		<div class="login-box">
-			<SwitchDark class="dark" />
-			<div class="login-left">
-				<img src="@/assets/images/login_left11.png" alt="login" />
-			</div>
-			<div class="login-form">
-				<div class="login-logo">
-					<img class="login-icon" src="@/assets/images/logo.png" alt="" />
-					<h2 class="logo-text">ChangLi</h2>
-				</div>
-				<LoginForm />
-			</div>
-		</div>
-	</div>
+	<div>Waiting...</div>
 </template>
 
 <script setup lang="ts" name="login">
-import SwitchDark from "@/components/SwitchDark/index.vue"
-import LoginForm from "./components/LoginForm.vue"
+import { onMounted } from "vue"
+import { useRouter } from "vue-router"
+import { ElNotification } from "element-plus"
+import { GlobalStore } from "@/stores"
+import { TabsStore } from "@/stores/modules/tabs"
+import { KeepAliveStore } from "@/stores/modules/keepAlive"
+import { getTimeState } from "@/utils/util"
+import { HOME_URL } from "@/config/config"
+import { initDynamicRouter } from "@/routers/modules/dynamicRouter"
+import AuthService from "@/services/auth.service"
+
+const router = useRouter()
+const tabsStore = TabsStore()
+const keepAlive = KeepAliveStore()
+const globalStore = GlobalStore()
+const auth = new AuthService()
+
+onMounted(() => {
+	auth.getUser().then(async (user: any) => {
+		if (user && user !== null && !user.expired) {
+			globalStore.setToken(user.access_token)
+			// 2.添加动态路由
+			await initDynamicRouter()
+			// 3.清空 tabs、keepAlive 保留的数据
+			tabsStore.closeMultipleTab()
+			keepAlive.setKeepAliveName()
+			// 4.跳转到首页
+			router.push(HOME_URL)
+			ElNotification({
+				title: getTimeState(),
+				message: "欢迎登录 南荣基础框架",
+				type: "success",
+				duration: 3000
+			})
+		} else {
+			// 1.执行登录接口
+			await auth.login()
+		}
+	})
+})
 </script>
 
-<style scoped lang="scss">
-@import "./index.scss";
-</style>
+<style scoped lang="scss"></style>
